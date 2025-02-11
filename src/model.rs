a16eff896586dde43294e04e1f4812d5093c4e27
Mikael Souza <mikael.souza.cc@gmail.com>
2025-02-11 00:55:59 -0300

    fix updated dependency incompatibilities

--- src/model.rs
+++ src/model.rs
@@ -1,25 +1,25 @@
 use crate::structs::ModelParams;
 use axum::extract::Query;
 use ndarray::Array2;
-use ort;
-
+use ort::session::{Session, SessionOutputs};
+use ort::execution_providers::CPUExecutionProvider;
 pub struct Model {
-    pub session: ort::Session,
+    pub session: Session,
 }
 
 impl Model {
     pub fn new() -> Self {
         Model {
-            session: ort::Session::builder()
+            session: Session::builder()
                 .unwrap()
-                .with_execution_providers([ort::CPUExecutionProvider::default().build()])
+                .with_execution_providers([CPUExecutionProvider::default().build()])
                 .unwrap()
                 .commit_from_file("model/logreg_iris.onnx")
                 .unwrap(),
         }
     }
 
-    pub fn run_model(&self, data: Query<ModelParams>) -> ort::SessionOutputs {
+    pub fn run_model(&self, data: Query<ModelParams>) -> SessionOutputs {
         let input_data = vec![[data.val1, data.val2, data.val3, data.val4]];
         let input_array = Array2::from(input_data);
         let input_tensor = ort::inputs![input_array].unwrap();
